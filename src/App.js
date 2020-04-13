a54314b88198195c35148de873da399e0a11861f
Kunal Pawar <kunal1620@gmail.com>
2020-04-13 22:31:54 +1000

    Added new page and components 1. Added Shop page 2. Added CollectionPreview component and CollectionItem component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
-import Homepage from "./pages/homepage/homepage";
 import { Switch, Route } from "react-router-dom";
+
+import Homepage from "./pages/homepage/homepage";
+import ShopPage from "./pages/shop/shop-page";
 
 import "./App.css";
 
@@ -11,7 +13,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
