e13e6ce7e0dc5ea7b7c9ca798979913b16674815
Kunal Pawar <kunal1620@gmail.com>
2020-06-12 23:51:43 +1000

    Added add and remove items from cart with clearing item on remove if only one was left

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,5 +1,5 @@
 import { cartActionTypes } from "./cart-types";
-import { addItemCart } from "./cart-utils";
+import { addItemToCart, removeItemFromCart } from "./cart-utils";
 
 const INITIAL_STATE = {
   isCartHidden: true,
@@ -17,7 +17,21 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case cartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: addItemCart(state.cartItems, action.payload),
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+
+    case cartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
+      };
+
+    case cartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
       };
 
     default:
--- src/redux/cart/cart-utils.js
+++ src/redux/cart/cart-utils.js
@@ -1,4 +1,4 @@
-export const addItemCart = (cartItems, cartItemToAdd) => {
+export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingItem = cartItems.find(
     (cartItem) => cartItem.id === cartItemToAdd.id
   );
@@ -15,4 +15,22 @@ export const addItemCart = (cartItems, cartItemToAdd) => {
   // If item is not present in cart add it to the cart
   // with an quantity key
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  if (existingItem.quantity === 1) {
+    // If last item in cart, remove it
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  // If more than one items in cart, decrease quantity by 1
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
